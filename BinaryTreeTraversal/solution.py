b563e226a22636128a24d1beee50de2a4930ef8a
Yurii Fito <fito.pn@ucu.edu.ua>
2025-04-16 11:39:18 +0300

    Add directories for problems; add some functions to BinaryTreeTravels

---
+++ BinaryTreeTraversal/solution.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data, left = None, right = None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+# Pre-order traversal
+def pre_order(node: Node):
+    output = []
+    if not isinstance(node, Node):
+        return []
+    _pre_order(node, output)
+    return output
+
+def _pre_order(node: Node, output: list):
+    output.append(node.data)
+    if node.left:
+        _pre_order(node.left, output)
+    if node.right:
+        _pre_order(node.right, output)
+    return output
+
+# In-order traversal
+def in_order(node):
+    if not isinstance(node, Node):
+        return []
+    output = []
+    _in_order(node, output)
+    return output
+
+def _in_order(node: Node, output: list):
+    if node.left:
+        _in_order(node.left, output)
+    output.append(node.data)
+    if node.right:
+        _in_order(node.right, output)
+
+# Post-order traversal
+def post_order(node):
+    return []
+
