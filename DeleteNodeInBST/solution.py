1c35a1d7b4b6c90dde8867da626fbe3a0818fa94
Yurii Fito <fito.pn@ucu.edu.ua>
2025-04-19 13:01:54 +0300

    Add solution to DeleteNodeInBST; Fix directory names; Add photo proof to problem 3

--- DeleteNodeInBST/solution.py
+++ DeleteNodeInBST/solution.py
@@ -1,25 +1,34 @@
-class Node:
-    def __init__(self, L, R, n):
-        self.left = L
-        self.right = R
-        self.value = n
+# Definition for a binary tree node.
 
+class TreeNode:
+    def __init__(self, val=0, left: 'TreeNode'=None, right: 'TreeNode'=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-def tree_by_levels(node):
-    if not isinstance(node, Node):
-        return []
-    output = []
+class Solution:
+    def deleteNode(self, root: TreeNode | None, key: int) -> TreeNode | None:
+        if not root:
+            return None
 
-    level = [node]
-    while level:
-        output.extend(map(lambda x: x.value, level))
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            cur = root.right
+            while cur.left:
+                cur = cur.left
+            min_larger_val = cur.val
+            root.val = min_larger_val
+            root.right = self.deleteNode(root.right, min_larger_val)
 
-        children = []
-        for member in level:
-            if member.left:
-                children.append(member.left)
-            if member.right:
-                children.append(member.right)
-        level = children
+        return root
 
-    return output
+# # [5,3,6,2,4,null,7]
+# a = Solution()
+# b = a.deleteNode(TreeNode(3, TreeNode(1, TreeNode(0.5), TreeNode(2)), TreeNode(4)), 1)
